d794744194b8575b9c9c571721f1d4cc8895b943
Youzi <youzi9601@users.noreply.github.com>
2026-06-12 01:34:55 +0800

    fix(security): escape XML entities in xml-formatter and add workflow permissions

--- packages/reporter-junit/src/xml-formatter.ts
+++ packages/reporter-junit/src/xml-formatter.ts
@@ -42,10 +42,10 @@ export interface JUnitTestSuites {
 
 function escapeXml(str: string): string {
   return str
-    .replace(/&/g, '&')
-    .replace(/</g, '<')
-    .replace(/>/g, '>')
-    .replace(/"/g, '"')
+    .replace(/&/g, '&amp;')
+    .replace(/</g, '&lt;')
+    .replace(/>/g, '&gt;')
+    .replace(/"/g, '&quot;')
     .replace(/'/g, '&apos;')
 }
 
